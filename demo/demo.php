be2dddabc43ebcac32bfc28e10836763d4e641d1
sahil-sharma-imark <sahil.sharma.imark@gmail.com>
2023-02-02 17:53:02 +0530

    demo.php upload

---
+++ demo/demo.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Laravel
+ * @author   Taylor Otwell <taylor@laravel.com>
+ */
+
+$uri = urldecode(
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) ?? ''
+);
+
+echo "vikas kumar";
+
+require_once __DIR__.'/public/server.php';
